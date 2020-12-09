0395e6f86e02460ec2ff5a6712e972ca4ee29d8a
Wil Taylor <cert@wiltaylor.dev>
2020-12-09 11:02:30 +1000

    Removed dependancy on click-alias as it was breaking stuff

--- wtdevtool/dev.py
+++ wtdevtool/dev.py
@@ -1,23 +1,23 @@
 import click
 import platform
 import os
-from click_aliases import ClickAliasedGroup
+#from click_aliases import ClickAliasedGroup
 
-def parse_repo(slug):
-    if slug.startwith('gh'):
+#def parse_repo(slug):
+#    if slug.startwith('gh'):
 
-@click.group(cls=ClickAliasedGroup)
-def cli():
-    pass
+#@click.group(cls=ClickAliasedGroup)
+#def cli():
+#    pass
 
-@cli.command(aliases=['o', 'checkout'])
-@click.argument('repo')
-def open(repo):
-    print('open')
+#@cli.command(aliases=['o', 'checkout'])
+#@click.argument('repo')
+#def open(repo):
+#    print('open')
 
-@cli.command(aliases=['new', 'c'])
-@click.argument('name')
-def create(name):
-    print('new')
+#@cli.command(aliases=['new', 'c'])
+#@click.argument('name')
+#def create(name):
+#    print('new')
 
 
